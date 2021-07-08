e683332a5d274becd5244109de018488c256e66e
Omar <omar.branez@gmail.com>
2021-07-08 01:51:42 -0400

    add party controller and views

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -1,3 +1,21 @@
 class PartyController < ApplicationController
     
+    def index
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
 end
