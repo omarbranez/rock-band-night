0b9e522199ab273feb2195f2400b0a2284d2d3ce
Omar <omar.branez@gmail.com>
2021-07-20 15:05:59 -0400

    fix issue where user validations prevented omniauth logins

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
     has_many :user_songs
     has_many :songs, through: :user_songs
-    validates_presence_of :username
-    validates_uniqueness_of :username
-    # validates :email, :unique => true
+    validates_presence_of :username, on: :create
+    validates_uniqueness_of :username, on: :create
+    validates_format_of :username, with: /(?=.*([a-z]|[A-Z]))/, on: :update#, message: "Username must contain at least one letter" }
+    validates_uniqueness_of :email, on: :create
+    after_create :remove_spaces_from_username
     has_secure_password
 
 
@@ -15,6 +17,12 @@ class User < ActiveRecord::Base
         current_user == User.find(1)
     end
     
+    def remove_spaces_from_username
+        if self.username.match(/\s/)
+            self.update(username: self.username.gsub(/\s/, ""))
+        end
+    end
+
     private
     
     def self.create_by_omniauth(auth)
